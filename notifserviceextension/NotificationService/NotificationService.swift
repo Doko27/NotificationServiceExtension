593c5b99de2236c07a92702cfc8946b90b9cd205
IrfanTriHandoko <trihandokoirfan@gmail.com>
2020-07-28 09:35:20 +0700

    self annotation

--- notifserviceextension/NotificationService/NotificationService.swift
+++ notifserviceextension/NotificationService/NotificationService.swift
@@ -15,9 +15,9 @@ class NotificationService: UNNotificationServiceExtension {
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        self.bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
-        guard let bestAttemptContent = bestAttemptContent else { return }
+        guard let bestAttemptContent = self.bestAttemptContent else { return }
         // Modify the notification content here...
         bestAttemptContent.title = "\(bestAttemptContent.title) [Modified]"
         // Save notification data to UserDefaults
